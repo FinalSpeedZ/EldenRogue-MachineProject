1164092ef93e408e54995a7b3b86934080a83c55
Lance <lance_d_ong@dlsu.edu.ph>
2022-11-09 20:50:12 +0800

    09-11-2022 Edits to title screen file

--- screens/titlescreen.c
+++ screens/titlescreen.c
@@ -1,3 +1,13 @@
-void openTitleScreen {
-	
+void openTitleScreen () {
+	printTitleScreen();
+}
+
+
+
+
+void printTitleScreen () {
+	printf("\n\n\t\t\t\tELDEN ROGUE");
+	printf("\n\n\t\t\t\t[1] Start");
+	printf("\n\t\t\t\t[2] Continue");
+	printf("\n\t\t\t\t[0] Exit");
 }
