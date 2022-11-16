3355f4fbef491d9fa29da9ec9d08e0611bf21a04
FinalSpeedZ <85392584+FinalSpeedZ@users.noreply.github.com>
2022-11-17 03:02:11 +0800

    Updated driver.c

--- driver.c
+++ driver.c
@@ -1,7 +1,14 @@
 #include "stdio.h"
 #include "stdlib.h"
-#include "screens/titlescreen.c"
-int main (){
+#include "string.h"
+#include "Windows.h"
+
+#include "utility/printer.c"
+#include "utility/scanner.c"
+
+#include "EldenRogueScreens/title_screen.c"
+
+int main() {
 	openTitleScreen();
 	return 0;
 }
